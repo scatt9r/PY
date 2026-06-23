5661799d956c2d31bc6332796351da6d8b3fb2dc
scatt9r <2572874937@qq.com>
2026-06-23 11:15:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hi")
+def main():
+    print("=== BMI 건강 계산기 ===")
+
+    try:
+        name = input("이름: ")
+        height = float(input("키(cm): "))
+        weight = float(input("현재 몸무게(kg): "))
+        target = float(input("목표 몸무게(kg): "))
+        
+        my_bmi = check_bmi(weight, height)
+        status = get_status(my_bmi)
